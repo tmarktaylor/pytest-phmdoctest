13f1b443f1dde620aa75694652c04014488a32a5
tmarktaylor <24257134+tmarktaylor@users.noreply.github.com>
2021-11-10 14:27:02 -0500

    Bump version to 0.0.2.

--- conf.py
+++ conf.py
@@ -16,7 +16,7 @@ copyright = "2021, Mark Taylor"
 author = "Mark Taylor"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.1"
+release = "0.0.2"
 
 
 # -- General configuration ---------------------------------------------------
--- index.rst
+++ index.rst
@@ -2,7 +2,7 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-pytest-phmdoctest 0.0.1
+pytest-phmdoctest 0.0.2
 =======================
 
 .. toctree::
